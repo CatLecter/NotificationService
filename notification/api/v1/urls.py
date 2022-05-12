648fea8cf3a12e83c3f6223f548b5a794bd1cdbe
Oleg A <t0rr@mail.ru>
2022-05-12 19:18:31 +0300

    style: apply black

--- notification/api/v1/urls.py
+++ notification/api/v1/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from api.v1.views import *
 
 urlpatterns = [
-    path('ugc_event/', UGCEventAPIView.as_view()),
+    path("ugc_event/", UGCEventAPIView.as_view()),
 ]
--- notification/manager_panel/apps.py
+++ notification/manager_panel/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ManagerPanelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manager_panel'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "manager_panel"
