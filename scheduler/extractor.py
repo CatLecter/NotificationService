c0bafacd9b49ee7beffb64398770c4a3880ff099
Vasilii Safronov <vasi1988@mail.ru>
2022-05-14 20:32:12 +0300

    added event loader

---
+++ scheduler/extractor.py
@@ -0,0 +1,28 @@
+import backoff
+from psycopg2 import OperationalError
+from psycopg2.extensions import connection
+
+
+class EventExtractor:
+    """Класс для выгрузки событий из PostgreSQL."""
+
+    def __init__(self, pg_conn: connection):
+        self.cursor = pg_conn.cursor()
+
+    @backoff.on_exception(backoff.expo, OperationalError, max_tries=5)
+    def get_event_uuids(self, event_type: str, limit: int) -> tuple:
+        self.cursor.execute(
+            f"""SELECT notification_id FROM events
+            WHERE event_type = '{event_type}'
+            AND in_queue = FALSE LIMIT {limit}"""
+        )
+        list_uuids = self.cursor.fetchall()
+        tuple_uuids = tuple([str(*_) for _ in list_uuids])
+        return tuple_uuids
+
+    @backoff.on_exception(backoff.expo, OperationalError, max_tries=5)
+    def update_event_uuids(self, tuple_uuids: tuple) -> None:
+        self.cursor.execute(
+            f"""UPDATE events SET in_queue = TRUE
+            WHERE notification_id IN {tuple_uuids}"""
+        )
--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -1,10 +1,11 @@
-import backoff
 import psycopg2
 from celery import Celery
-from config import BROKER_URL, EVENT_LIMIT, EVENT_PRIORITY, PG_DSN, log_config
+from config import AMQP_URL, BROKER_URL, EVENT_LIMIT, EVENT_PRIORITY, PG_DSN, log_config
+from extractor import EventExtractor
+from loader import EventLoaderToQueue
 from loguru import logger
+from pika import BlockingConnection, URLParameters
 from psycopg2 import DatabaseError, OperationalError
-from psycopg2.extensions import connection
 from psycopg2.extras import DictCursor
 
 logger.add(**log_config)
@@ -12,52 +13,32 @@ logger.add(**log_config)
 celery_app = Celery("tasks", broker=BROKER_URL)
 
 
-class EventExtractor:
-    """Класс для выгрузки событий из PostgreSQL."""
-
-    def __init__(self, pg_conn: connection):
-        self.cursor = pg_conn.cursor()
-
-    @backoff.on_exception(backoff.expo, OperationalError, max_tries=5)
-    def get_event_uuids(self, event_type: str, limit: int):
-        self.cursor.execute(
-            f"""SELECT notification_id FROM events
-            WHERE event_type = '{event_type}'
-            AND in_queue = FALSE LIMIT {limit}"""
-        )
-        event_uuids = self.cursor.fetchall()
-        if event_uuids:
-            tuple_uuids = tuple([str(*_) for _ in event_uuids])
-            self.cursor.execute(
-                f"""UPDATE events SET in_queue = TRUE
-                WHERE notification_id IN {tuple_uuids}"""
-            )
-            print(tuple_uuids)
-            return event_uuids
-
-
 @celery_app.task
 def transfer_events():
-    """
-    Задача чтения UUID событий из базы.
-    """
+    """Задача трансфера notification_uuid из PostgreSQL в очередь событий RabbitMQ."""
 
     try:
         with psycopg2.connect(
             dbname=PG_DSN["dbname"],
             user=PG_DSN["user"],
             password=PG_DSN["password"],
-            host="postgres",
+            host=PG_DSN["host"],
             cursor_factory=DictCursor,
-        ) as pg_conn:
+        ) as pg_conn, BlockingConnection(
+            URLParameters(AMQP_URL),
+        ) as amqp_conn:
             pg = EventExtractor(pg_conn=pg_conn)
-            pg.get_event_uuids(EVENT_PRIORITY, EVENT_LIMIT)
+            uuids = pg.get_event_uuids(EVENT_PRIORITY, EVENT_LIMIT)
+            if uuids:
+                amqp = EventLoaderToQueue(conn=amqp_conn)
+                amqp.send_ids(uuids)
+                pg.update_event_uuids(uuids)
     except (OperationalError, DatabaseError) as e:
         logger.exception(e)
 
 
 @celery_app.on_after_configure.connect
 def setup_periodic_taskc(sender, **kwargs):
-    """Планировщик запуска процессов (каждые 5 секунд)."""
+    """Планировщик запуска процессов (каждые 10 секунд)."""
 
-    sender.add_periodic_task(5.0, transfer_events.s())
+    sender.add_periodic_task(10.0, transfer_events.s())
